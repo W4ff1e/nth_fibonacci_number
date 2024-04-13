3cc20f27c2c2f5b8947d61d6c5a9fbed1240a916
W4ff1e <72278085+W4ff1e@users.noreply.github.com>
2024-04-13 15:19:51 +0000

    Optimized calculations and implemented BigInt

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
+use num_bigint::BigUint;
 use std::io;
-
 fn main() {
     println!("I don't know math so if you want to be *technically* correct, please use 0 and 1 as your seed values.");
     loop {
         let first_seed = get_seed("first");
 
-        let first_seed: usize = match first_seed.trim().parse() {
+        let first_seed: BigUint = match first_seed.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
         let second_seed = get_seed("second");
 
-        let second_seed: usize = match second_seed.trim().parse() {
+        let second_seed: BigUint = match second_seed.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
@@ -24,11 +24,15 @@ fn main() {
             .read_line(&mut nth)
             .expect("Failed to read line!");
 
-        let nth: usize = match nth.trim().parse() {
+        let nth: i64 = match nth.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
+        let result = calculate(first_seed, second_seed, nth);
+
+        println!("The {nth} digit off your fibonacci sequence is: {result}")
+        /*
         let mut sequence = vec![first_seed, second_seed];
 
         for digit in 1..nth + 1 {
@@ -44,6 +48,7 @@ fn main() {
             // Test Code: (aaron-bond.better-comments)
             //// println!("Debug: Iteration: {digit}, Result: {}", sequence[digit + 1]);
         }
+         */
     }
 }
 
@@ -55,4 +60,13 @@ fn get_seed(seed_number: &str) -> String {
         .read_line(&mut seed)
         .expect("Failed to read line!");
     seed
+}
+
+fn calculate(mut f0: BigUint, mut f1: BigUint, digits_to_calc: i64) -> BigUint {
+    for _ in 0..digits_to_calc {
+        let f2 = f0 + &f1;
+        f0 = f1;
+        f1 = f2;
+    }
+    f0
 }
